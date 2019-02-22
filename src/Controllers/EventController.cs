cf1aad172b2d5b346d955535a18917d1d8cc571c
Thomas Geske <thomasge@microsoft.com>
2019-02-22 11:41:43 +0100

    added mongo db query.

--- src/Controllers/EventController.cs
+++ src/Controllers/EventController.cs
@@ -3,13 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 
 namespace Microsoft.WWV.Controllers
 {
     [Route("api/[controller]")]
     public class EventController : Controller
     {
+        
+        [HttpGet("[action]")]
+        public IEnumerable<Event> GetEvents()
+        {
+            return TestEventGenerator.GetSampleEvents();
+        }
 
+        [HttpGet("[action]")]
+        public IEnumerable<Event> GetAllEvents()
+        {
+            MongoClient _client = new MongoClient("mongodb://wwv-dev:ifFiwhFGGlIExJp7KDbxjjHmTnW2zumcfDsqxbC23PfAZePpgos7AHR93lpihgTt7bMR8XlJp1X4yFtpqc780g==@wwv-dev.documents.azure.com:10255/?ssl=true&replicaSet=globaldb");
+            MongoServer _server = _client.GetServer();
+            MongoDatabase _db = _server.GetDatabase("voluntdb");   
+            return _db.GetCollection<Event>("events").FindAll();
+        }
+    }
 
+    internal static class TestEventGenerator
+    {
+        static IEnumerable<Event> _events;
+
+        internal static IEnumerable<Event> GetSampleEvents()
+        {
+            return _events;
+        }
+
+        static TestEventGenerator()
+        {
+            var events = new List<Event>();
+            _events = events;
+            events.Add(
+                new Event(){
+                    Name = "aa",
+                    Description = "bb"
+                }
+            );
+        } 
     }
 }
---
+++ src/Models/Event.cs
@@ -0,0 +1,26 @@
+namespace Microsoft.WWV
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Country { get; set; }
+        public string OwnerName1 {get;set;}
+        public string OwnerName2 {get;set;}
+        public string Company {get;set;}
+        public string EventType {get;set;}
+        public string Eventdate {get;set;}
+        public string EventLocation {get;set;}
+        public string Url {get;set;}
+    }
+
+    public class Registration
+    {
+        /*
+        
+
+
+
+         */
+    }
+}
