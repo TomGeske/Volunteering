a6eac4cf21ab64742323775485d93bdcd273cf09
Thomas Geske <thomasge@microsoft.com>
2019-03-09 18:35:15 +0100

    added FetchEvents component

---
+++ src/ClientApp/src/components/FetchEvents.js
@@ -0,0 +1,52 @@
+﻿import React, { Component } from 'react';
+
+export class FetchEvents extends Component {
+    static renderEventsTable(_events) {
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th>Owner</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {_events.map(event =>
+                        <tr key={event.name}>
+                            <td>{event.name}</td>
+                            <td>{event.eventLocation}</td>
+                            <td>{event.ownerName1}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+    displayName = FetchEvents.name
+
+    constructor(props) {
+        super(props);
+        this.state = { events: [], loading: true };
+
+        fetch('api/Event/GetEvents')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ events: data, loading: false });
+            });
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : FetchEvents.renderEventsTable(this.state.events);
+
+        return (
+            <div>
+                <h1>Events</h1>
+                <p>All upcoming events.</p>
+                {contents}
+            </div>
+        );
+    }
+}
