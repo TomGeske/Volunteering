9951fdefcf2a49c5932a1e7836b1d962c46cc8a7
Thomas Geske <thomasge@microsoft.com>
2019-06-06 11:14:27 +0200

    added a footer

--- src/ClientApp/src/components/Footer.js
+++ src/ClientApp/src/components/Footer.js
@@ -9,7 +9,7 @@ export class Footer extends Component {
     return (
       <div>
         {/* Footer */}
-        <footer className="page-footer font-small blue pt-4">
+        <footer className="footer font-small mt-auto pt-4">
           {/* Footer Links */}
           <div className="container-fluid text-center text-md-left">
             {/* Grid row */}
@@ -17,23 +17,38 @@ export class Footer extends Component {
               {/* Grid column */}
               <div className="col-md-6 mt-md-0 mt-3">
                 {/* Content */}
-                <h5 className="text-uppercase">Footer Content</h5>
-                <p>Here you can use rows and columns to organize your footer content.</p>
+                <h5 className="text-uppercase">Do you have feedback? </h5>
+                <p>Please let us know <a href="https://github.com/TomGeske/Volunteering/issues">here</a></p>
+                {/* Content */}
               </div>
-            </div>
-            {/* Grid row */}
-          </div>
-          {/* Footer Links */}
-          {/* Copyright */}
-          <div className="footer-copyright text-center py-3">
-            © 2019 Microsoft
-          </div>
-          {/* Copyright */}
-        </footer>
-        {/* Footer */}
-      </div>
-    );
-  }
-}
+              {/* Grid column */}
+              <hr class="clearfix w-100 d-md-none pb-3" />
+              {/* Grid column */}
+              <div class="col-md-3 mb-md-0 mb-3">
+              </div>
+              {/* Grid column */}
 
-export default withAITracking(ai.reactPlugin, Footer);
+              {/* Grid column */}
+              <div class="col-md-3 mb-md-0 mb-3">
+                <span class="text-muted">
+                  Microsoft confidential. For use only by Microsof​t employees and approved vendors working on​​​behalf of​​ Microsoft.<a href="https://microsoft.sharepoint.com/teams/msdpn/sitepages/default.aspx">Data protection notice​​.</a>
+                </span>
+              </div>
+                  {/* Grid column */}
+            </div>
+                {/* Grid row */}
+              </div>
+              {/* Footer Links */}
+              {/* Copyright */}
+              <div className="footer-copyright text-center py-3">
+                &copy; Microsoft {new Date().getFullYear()}
+              </div>
+              {/* Copyright */}
+        </footer>
+            {/* Footer */}
+          </div>
+          );
+        }
+      }
+      
+      export default withAITracking(ai.reactPlugin, Footer);
--- src/ClientApp/src/index.js
+++ src/ClientApp/src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
