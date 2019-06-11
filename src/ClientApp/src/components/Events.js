e0dd26cfc0f56129294f1d7fef96bc674c82c10b
Lukasz Miroslaw <lumirosl@microsoft.com>
2019-06-11 17:12:39 +0200

    Replaced <table> with grid layout from react-bootstrap

--- src/ClientApp/src/components/Events.js
+++ src/ClientApp/src/components/Events.js
@@ -3,36 +3,41 @@ import { ai } from '../TelemetryService';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import config from '../Config'
+import { Container, Row, Col, Button } from 'reactstrap';
 import { ReactBingmaps } from 'react-bingmaps';
 
 export class Events extends Component {
   static renderEventsTable(_events) {
     return (
-      <table className='table striped bordered hover'>
-        <tbody>
-          {_events.map(event => (
-            <tr key={event.id}>
-              <td><h3>{event.name}</h3>
-                Date: {event.eventdate}   
-                <a className="btn btn-primary mb1 bg-green" target="_blank" href="https://aka.ms/wwv-new-events" role="button">Register</a>
-                Event Type: tbd
-              </td>
-              <td><b>Meeting Point Location:</b> {event.eventLocation}, {event.country}
-                <div className="map-small">
-                  <ReactBingmaps id={event.id} bingmapKey={config.BING_API_KEY} mapOptions={{ 'showLocateMeButton': false, 'showMapTypeSelector': false}}
+      <Container>
+        
+        {_events.map(_event => (
+          
+        <Row>
+          <Col xs={6}  md={4} width="140px">
+            <h3>{_event.name}</h3>
+            <b>Date:</b> {new Date(_event.eventdate).toLocaleDateString()} - {new Date(_event.eventEndDate).toLocaleDateString()}
+            <p>Event organizer: tbd</p>
+            <p>Event type: tbd</p>
+          </Col>
+          <Col xs={6} md={4}>
+          <a className="btn btn-primary mb1 bg-green" target="_blank" href="https://aka.ms/wwv-new-events" role="button">Register</a>
+          <br/>
+            <Link to={{
+                  pathname: '/eventdetails/' + _event.id
+                }}>Details</Link>
+          </Col>
+          <Col xs={6} md={4}>
+          <div className="map-small">
+                  <ReactBingmaps id={_event.id} bingmapKey={config.BING_API_KEY} mapOptions={{ 'showLocateMeButton': false, 'showMapTypeSelector': false}}
                     className="map-small" />
                 </div>
-              </td>
-              <td>
-                <Link to={{
-                  pathname: '/eventdetails/' + event.id
-                }}>Details</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+          </Col>
+          
+        </Row>
+        ))}
+        </Container>
+    )
   }
   displayName = Events.name
 
