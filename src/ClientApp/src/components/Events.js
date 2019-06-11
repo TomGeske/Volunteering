1e14ba4052d7a837ff2d08ef489e15b7d643abb3
Lukasz Miroslaw <lumirosl@microsoft.com>
2019-06-11 12:39:34 +0200

    Updated the td and tr tags

--- src/ClientApp/src/components/Events.js
+++ src/ClientApp/src/components/Events.js
@@ -10,23 +10,19 @@ export class Events extends Component {
   static renderEventsTable(_events) {
     return (
       <table className='table striped bordered hover'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date</th>
-            <th>Location</th>
-            <th>Register</th>
-          </tr>
-        </thead>
         <tbody>
           {_events.map(event => (
             <tr key={event.id}>
-              <td>{event.name}</td>
-              <td>{event.eventdate}</td>
+              <td><h3>{event.name}</h3>
+                Date: {event.eventdate}   
+                <a className="btn btn-primary mb1 bg-green" target="_blank" href="https://aka.ms/wwv-new-events" role="button">Register</a>
+                Event Type: tbd
+              </td>
               <td><b>Meeting Point Location:</b> {event.eventLocation}, {event.country}
-              <div className="map-small">            
-                <ReactBingmaps id={event.id} bingmapKey={config.BING_API_KEY} className="map-small"/>            
-          </div>
+                <div className="map-small">
+                  <ReactBingmaps id={event.id} bingmapKey={config.BING_API_KEY}
+                    className="map-small" />
+                </div>
               </td>
               <td>
                 <Link to={{
@@ -43,10 +39,10 @@ export class Events extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       events: [], loading: true,
       bingmapkey: "Aqa_swdrZ1-tT4mMMKHhlrYFXfVdYh8u1DxmHcVjBCAEgsUo_SpPR5aKG4roSYrz",
-      
+
     };
 
     fetch('api/Event/')
@@ -65,7 +61,7 @@ export class Events extends Component {
       <div>
         <h1 className="text-center">Upcoming events</h1>
         <a className="btn btn-primary btn-lg" target="_blank" href="https://aka.ms/wwv-new-events" role="button">Create event »</a>
-        <br />        
+        <br />
         {contents}
       </div>
     );
