90420283a4d2026c760c9ef893ee9d0df11cc559
Thomas Geske <thomasge@microsoft.com>
2019-06-12 13:17:47 +0200

    added event Owner & eventType fields

--- src/ClientApp/src/components/Events.js
+++ src/ClientApp/src/components/Events.js
@@ -10,33 +10,33 @@ export class Events extends Component {
   static renderEventsTable(_events) {
     return (
       <Container>
-        
+
         {_events.map(_event => (
-          
-        <Row>
-          <Col xs={6}  md={4} width="140px">
-            <h3>{_event.name}</h3>
-            <b>Date:</b> {new Date(_event.eventdate).toLocaleDateString()} - {new Date(_event.eventEndDate).toLocaleDateString()}
-            <p>Event organizer: tbd</p>
-            <p>Event type: tbd</p>
-          </Col>
-          <Col xs={6} md={4}>
-          <a className="btn btn-primary mb1 bg-green" target="_blank" href="https://aka.ms/wwv-new-events" role="button">Register</a>
-          <br/>
-            <Link to={{
-                  pathname: '/eventdetails/' + _event.id
-                }}>Details</Link>
-          </Col>
-          <Col xs={6} md={4}>
-          <div className="map-small">
-                  <ReactBingmaps id={_event.id} bingmapKey={config.BING_API_KEY} mapOptions={{ 'showLocateMeButton': false, 'showMapTypeSelector': false}}
-                    className="map-small" />
-                </div>
-          </Col>
-          
-        </Row>
+
+          <Row>
+            <Col xs={6} md={4} width="140px">
+              <h3>{_event.name}</h3>
+              <b>Date:</b> {new Date(_event.eventdate).toLocaleDateString()} - {new Date(_event.eventEndDate).toLocaleDateString()}
+              <p>Event organizer: {_event.ownerName1} {_event.ownerName2}</p>
+              <p>Event type: {_event.eventType}</p>
+            </Col>
+            <Col xs={6} md={4}>
+              <a className="btn btn-primary mb1 bg-green" target="_blank" href="https://aka.ms/wwv-new-events" role="button">Register</a>
+              <br />
+              <Link to={{
+                pathname: '/eventdetails/' + _event.id
+              }}>Details</Link>
+            </Col>
+            <Col xs={6} md={4}>
+              <div className="map-small">
+                <ReactBingmaps id={_event.id} bingmapKey={config.BING_API_KEY} mapOptions={{ 'showLocateMeButton': false, 'showMapTypeSelector': false }}
+                  className="map-small" />
+              </div>
+            </Col>
+
+          </Row>
         ))}
-        </Container>
+      </Container>
     )
   }
   displayName = Events.name
