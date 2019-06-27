c1f411efe378740ede4082d3d4c7aeee6bd7b98b
Thomas Geske <thomasge@microsoft.com>
2019-06-27 12:30:34 +0200

    enable eslint

---
+++ src/ClientApp/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': ["error", "windows"]
+  },
+};
