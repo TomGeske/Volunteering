73c24f170dfc89f947e2a3f3f3df1e8e7c0adaa0
Hamlet D'Arcy <hamda@microsoft.com>
2019-07-16 14:51:53 +0200

    Fixing accessibility issues in web client

--- src/ClientApp/src/components/Footer.js
+++ src/ClientApp/src/components/Footer.js
@@ -17,10 +17,9 @@ export class Footer extends Component {
               {/* Grid column */}
               <div className="col-md-6 mt-md-0 mt-3">
                 {/* Content */}
-                <h5 className="text-uppercase">Do you have feedback? </h5>
+                <h2 className="feedback-header text-uppercase">Do you have feedback?</h2>
                 <p>
-                  Please let us know
-                  <a href="https://github.com/TomGeske/Volunteering/issues">here</a>
+                  Please <a class="footer-anchor" href="https://github.com/TomGeske/Volunteering/issues/new">file an issue</a>
                 </p>
                 {/* Content */}
               </div>
@@ -32,10 +31,10 @@ export class Footer extends Component {
 
               {/* Grid column */}
               <div className="col-md-3 mb-md-0 mb-3">
-                <span className="text-muted">
-                  Microsoft confidential. For use only by Microsof​t
+                <span>
+                  Microsoft confidential. For use only by Microsoft​
                   employees and approved vendors working on​​​behalf of​​ Microsoft.&nbsp;
-                  <a href="https://microsoft.sharepoint.com/teams/msdpn/sitepages/default.aspx">Data protection notice​​.</a>
+                  <a class="footer-anchor" href="https://microsoft.sharepoint.com/teams/msdpn/sitepages/default.aspx">Data protection notice​​.</a>
                 </span>
               </div>
               {/* Grid column */}
@@ -45,7 +44,7 @@ export class Footer extends Component {
           {/* Footer Links */}
           {/* Copyright */}
           <div className="footer-copyright text-center py-3">
-            &copy; Microsoft
+            &copy; Microsoft&nbsp;
             {new Date().getFullYear()}
           </div>
           {/* Copyright */}
